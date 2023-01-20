764849491b51a2538140a8cb89cf6b5d604bb106
ordinaryroad <or-mjz@qq.com>
2023-01-20 21:12:31 +0800

    chore: add LICENSE

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2021 苗锦洲
+ * Copyright (c) 2023 苗锦洲
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
